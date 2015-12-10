4fe60344048056a3dcf96045291923c190ad2b70
ryanniemann <ryanniemann@hotmail.com>
2015-12-09 21:26:37 -0700

    Plot1
    Plot 1 R script and PNG file.

---
+++ plot1.R
@@ -0,0 +1,17 @@
+#Read the power consumption dataset from Data folder in workgin directory 
+HPC_data <- read.csv("./Data/household_power_consumption.txt", 
+                     header=TRUE, sep=';', na.strings="?", 
+                     check.names=FALSE, stringsAsFactors=FALSE, 
+                     comment.char="", quote='\"')
+
+#Subset the data over the 2-day period in February, 2007
+data <- HPC_data[HPC_data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#Plot1
+hist(data$Global_active_power, main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+
+#Create and save the PNG file 
+dev.copy(png, file="plot1.png", height=480, width=480)
+dev.off()
+
